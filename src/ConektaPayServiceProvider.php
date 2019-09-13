492c8bda1621d5c8e1d3c7f9fbd6f87b983e00ea
Fabian Montero <fabian.montero@apscreativas.com>
2019-09-13 16:57:58 -0500

    Fix

--- src/ConektaPayServiceProvider.php
+++ src/ConektaPayServiceProvider.php
@@ -33,8 +33,8 @@ class ConektaPayServiceProvider extends ServiceProvider
             __DIR__ . '/config/conekta.php' => config_path('conekta.php'),
             __DIR__ . '/migrations/2019_08_28_161440_add_conekta_pay_to_users.php' => database_path('/migrations'.'/2019_08_28_161440'.'_add_conekta_pay_to_users.php'),
             __DIR__ . '/migrations/2019_08_28_183530_create_orders_table.php' => database_path('/migrations/2019_08_28_183530_create_orders_table.php'),
-            __DIR__ . '/events/ConektaEvent.php' => app_path('Events/ConektaEvent.php'),
-            __DIR__ . '/listeners/ConektaListener.php' => app_path('Listeners/ConektaListener.php'),
+            __DIR__ . '/events/ConektaEvent.ph' => app_path('Events/ConektaEvent.php'),
+            __DIR__ . '/listeners/ConektaListener.ph' => app_path('Listeners/ConektaListener.php'),
 
         ], 'conektapay');
     }
