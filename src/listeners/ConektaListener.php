d50f384ccc0b5064ef03125675ee4ab5ced35e01
Fabian Montero <fabian.montero@apscreativas.com>
2019-09-13 15:35:50 -0500

    Se implemento Event y Listener

--- src/listeners/ConektaListener.php
+++ src/listeners/ConektaListener.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\ConektaEvent;
+use Exception;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -26,5 +27,40 @@ class ConektaListener
      */
     public function handle(ConektaEvent $event)
     {
+        try {
+            switch ($event->request->type) {
+                case 'charge.created':
+                    # code...
+                    break;
+                case 'order.created':
+                    # code...
+                    break;
+                case 'charge.paid':
+                    # code...
+                    break;
+                case 'order.paid':
+                    $chargeable = config('conekta.chargeable');
+                    $chargeable = new $chargeable;
+                    $chargeable = $chargeable->where('conekta_order_id', $event->request->data['object']['id'])->update([
+                        'conekta_order_status' => $event->request->data['object']['payment_status']
+                    ]);
+                    // Custom paid logic
+                    break;
+                case 'order.pending_payment':
+                    # code...
+                    break;
+                case 'order.expired':
+                    # code...
+                    break;
+                case 'charge.refunded':
+                    # code...
+                    break;
+                default:
+                    return;
+                    break;
+            }
+        } catch (\Throwable $th) {
+            throw new Exception($th->getMessage());
+        }
     }
 }
--- src/routes/webhooks.php
+++ src/routes/webhooks.php
@@ -5,19 +5,6 @@ use Illuminate\Http\Request;
 
 Route::prefix('conekta')->group(function () {
     Route::post('webhook', function (Request $request) {
-        switch ($request->type) {
-            case 'order.paid':
-                $chargeable = config('conekta.chargeable');
-                $chargeable = new $chargeable;
-                $chargeable = $chargeable->where('conekta_order_id', $request->data['object']['id'])->first();
-                $chargeable->update([
-                    'conekta_order_status' => $request->data['object']['payment_status']
-                ]);
-                break;
-            default:
-                break;
-                
-            event(new ConektaEvent($request));
-        }
+        event(new ConektaEvent($request));
     })->name('conekta.webhook');
 });
